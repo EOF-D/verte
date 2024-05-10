8658b99e1fc746c1658cd509bce7db18494e49b2
EOF-D <END-OFD@pm.me>
2024-05-10 18:29:36 -0400

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Excerpt
+
+## Project Structure
+
+- `include/`: Header files.
+  - `excerpt/`: Project-specific header files.
+  - `excerpt_utils/`: Tools, i.e logging.
+- `src/`: Source code files.
+- `tests/`: Unit tests.
---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "A development environment for excerpt";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    defaultPackage =
+      forEachSystem (system: self.packages.${system}.excerpt-dev);
+
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        excerpt-dev = devenv.lib.mkShell {
+          inherit inputs pkgs;
+
+          modules = [
+            {
+              packages = with pkgs; [
+                clang-tools
+                cmake
+                zlib
+                gcc
+                gnumake
+                gtest
+                llvmPackages_17.libllvm
+              ];
+
+              pre-commit.hooks = {
+                clang-format.enable = true;
+                clang-tidy.enable = true;
+              };
+
+              scripts.excerpt-build.exec = ''
+                cmake -S . -B build
+                cmake --build build
+              '';
+
+              scripts.excerpt-tests.exec = ''
+                cmake -S . -B build -DBUILD_TESTS=true
+                cmake --build build
+                ./build/tests/excerpt-tests --gtest_brief=1
+              '';
+            }
+          ];
+        };
+      });
+  };
+}
