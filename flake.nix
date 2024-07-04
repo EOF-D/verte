fe1389544fe07713c2bfefdb28c952f43849dadd
EOF-D <END-OFD@pm.me>
2024-07-04 14:10:14 -0400

    feat: added support for `nix build` & installation

--- flake.nix
+++ flake.nix
@@ -21,50 +21,61 @@
   } @ inputs: let
     forEachSystem = nixpkgs.lib.genAttrs (import systems);
   in {
-    defaultPackage =
-      forEachSystem (system: self.packages.${system}.excerpt-dev);
+    packages = forEachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      vertec = pkgs.stdenv.mkDerivation {
+        name = "vertec";
+        src = ./.;
+        nativeBuildInputs = with pkgs; [cmake llvmPackages_17.llvm];
+        buildInputs = with pkgs; [llvmPackages_17.libllvm zlib];
+        cmakeFlags = ["-DINSTALL_VERTE=ON"];
+        installPhase = ''
+          cmake --install . --prefix $out
+        '';
+      };
+    });
 
-    devShells =
-      forEachSystem
-      (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        verte-dev = devenv.lib.mkShell {
-          inherit inputs pkgs;
+    defaultPackage = forEachSystem (system: self.packages.${system}.vertec);
+    devShells = forEachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      verte-dev = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          {
+            packages = with pkgs; [
+              clang-tools
+              cmake
+              doxygen
+              zlib
+              gcc
+              gnumake
+              gtest
+              llvmPackages_17.libllvm
+              valgrind
+              gdb
+              self.packages.${system}.vertec
+            ];
 
-          modules = [
-            {
-              packages = with pkgs; [
-                clang-tools
-                cmake
-                doxygen
-                zlib
-                gcc
-                gnumake
-                gtest
-                llvmPackages_17.libllvm
-                valgrind
-                gdb
-              ];
+            pre-commit.hooks = {
+              clang-format.enable = true;
+              clang-tidy.enable = true;
+            };
 
-              pre-commit.hooks = {
-                clang-format.enable = true;
-                clang-tidy.enable = true;
-              };
+            scripts.verte-build.exec = ''
+              cmake -S . -B build
+              cmake --build build
+            '';
 
-              scripts.verte-build.exec = ''
-                cmake -S . -B build
-                cmake --build build
-              '';
-
-              scripts.verte-tests.exec = ''
-                cmake -S . -B build -DBUILD_TESTS=true
-                cmake --build build
-                ./build/tests/verte-tests
-              '';
-            }
-          ];
-        };
-      });
+            scripts.verte-tests.exec = ''
+              cmake -S . -B build -DBUILD_TESTS=true
+              cmake --build build
+              ./build/tests/verte-tests
+            '';
+          }
+        ];
+      };
+    });
   };
 }
